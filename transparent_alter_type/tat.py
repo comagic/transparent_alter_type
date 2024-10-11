316252cf68386d4453bb5b345258986ee7d1fe77
Andrey Chernyakov <a.chernyakov@comagic.dev>
2024-10-11 16:46:36 +0300

    pip packet

--- transparent_alter_type/tat.py
+++ transparent_alter_type/tat.py
@@ -1,40 +1,12 @@
-#!/usr/bin/python3
-
 import sys
 import re
-import argparse
 import psycopg2
-from select import select
 import psycopg2.extras
-from psycopg2.extensions import POLL_OK, POLL_READ, POLL_WRITE
 import threading
 import queue
 import time
-import signal
 import datetime
 from pg_export.acl import acl_to_grants
-
-
-arg_parser = argparse.ArgumentParser(conflict_handler='resolve')
-arg_parser.add_argument('-h', '--host', required=True)
-arg_parser.add_argument('-p', '--port', type=int, required=True)
-arg_parser.add_argument('-d', '--dbname', required=True)
-arg_parser.add_argument('-t', '--table_name', required=True)
-arg_parser.add_argument('-c', '--column', action='append', help='column:new_type', default=[])
-arg_parser.add_argument('-j', '--jobs', type=int, required=True)
-arg_parser.add_argument('--force', action='store_true')
-arg_parser.add_argument('--cleanup', action='store_true')
-arg_parser.add_argument('--lock-timeout', type=int, default=5)
-arg_parser.add_argument('--time-between-locks', type=int, default=10)
-arg_parser.add_argument('--work-mem', type=str, default='1GB')
-arg_parser.add_argument('--min-delta-rows', type=int, default=10000)
-arg_parser.add_argument('--show-queries', action='store_true')
-arg_parser.add_argument('--skip-fk-validation', action='store_true')
-arg_parser.add_argument('--pgbouncer-host')
-arg_parser.add_argument('--pgbouncer-port', type=int)
-arg_parser.add_argument('--pgbouncer-pause-timeout', type=int, default=2)
-arg_parser.add_argument('--pgbouncer-time-between-pause', type=int, default=10)
-args = arg_parser.parse_args()
 
 
 class TAT:
@@ -183,7 +155,7 @@ class TAT:
                                   inner join pg_namespace icn
                                           on icn.oid = ic.relnamespace
                                   where i.indrelid = t.oid and
-                                        ic.relname not like '%%\_tat') i
+                                        ic.relname not like '%%\\_tat') i
               left join lateral (select uni.contype,
                                         array_agg(a.attname order by a.attnum) filter (where a.attnotnull) as pk_columns,
                                         count(1)
@@ -227,7 +199,7 @@ class TAT:
                                    from pg_constraint chk
                                   where chk.conrelid = t.oid and
                                         chk.contype = 'c' and
-                                        chk.conname not like '%%\_tat') chk
+                                        chk.conname not like '%%\\_tat') chk
              cross join lateral (select coalesce(array_agg(format('alter table %%s add constraint %%s %%s not valid;',
                                                                   fk.conrelid::regclass::text,
                                                                   fk.conname,
@@ -747,25 +719,3 @@ class TAT:
 
         print(self.table['name'], 'done in', self.duration(time.time() - start_time))
         print()
-
-def wait_select_inter(conn):
-    while 1:
-        try:
-            state = conn.poll()
-            if state == POLL_OK:
-                break
-            elif state == POLL_READ:
-                select([conn.fileno()], [], [])
-            elif state == POLL_WRITE:
-                select([], [conn.fileno()], [])
-            else:
-                raise conn.OperationalError(
-                    "bad state from poll: %s" % state)
-        except KeyboardInterrupt:
-            conn.cancel()
-            # the loop will be broken by a server error
-            continue
-psycopg2.extensions.set_wait_callback(wait_select_inter)
-
-t = TAT(args)
-t.run()
